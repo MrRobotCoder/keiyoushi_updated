a5638b6c0e3411b6474c9b170e51d9369c351948
Alberlando <alberlandoherculano@gmail.com>
2024-12-06 10:53:15 -0300

    alteração apk luratoon

---
+++ extensions-source/src/es/cerberusseries/src/eu/kanade/tachiyomi/extension/es/cerberusseries/CerberusSeries.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.es.cerberusseries
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class CerberusSeries : MangaThemesia(
+    "Cerberus Series",
+    "https://cerberuseries.xyz",
+    "es",
+) {
+    // Moved from custom to MangaThemesia
+    override val versionId = 2
+}
---
+++ extensions-source/src/fr/reaperscans/src/eu/kanade/tachiyomi/extension/fr/reaperscans/ReaperScans.kt
@@ -0,0 +1,13 @@
+package eu.kanade.tachiyomi.extension.fr.reaperscans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class ReaperScans : Keyoapp(
+    "Reaper Scans",
+    "https://reaper-scans.fr",
+    "fr",
+) {
+
+    // Migrated from Madara to Keyoapp.
+    override val versionId = 4
+}
